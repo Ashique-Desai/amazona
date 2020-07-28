5ffe6afe47661540e15e8be5581a15b9473c00d0
Ashique-Desai <desai.ashique@gmail.com>
2020-07-28 19:31:43 +0530

    init

---
+++ dist/data.js
@@ -0,0 +1,50 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  products: [{
+    _id: '1',
+    name: 'Slim Shirt',
+    category: 'Shirts',
+    image: '/images/d1.jpg',
+    price: 60,
+    brand: 'Nike',
+    rating: 4.5,
+    numReviews: 10,
+    countInStock: 6
+  }, {
+    _id: '2',
+    name: 'Fit Shirt',
+    category: 'Shirts',
+    image: '/images/d1.jpg',
+    price: 70,
+    brand: 'Nike',
+    rating: 5.5,
+    numReviews: 50,
+    countInStock: 15
+  }, {
+    _id: '3',
+    name: 'Best Pants',
+    category: 'Pants',
+    image: '/images/d1.jpg',
+    price: 150,
+    brand: 'Nike',
+    rating: 3.5,
+    numReviews: 70,
+    countInStock: 5
+  }, {
+    _id: '4',
+    name: 'Best Pants',
+    category: 'Pants',
+    image: '/images/d1.jpg',
+    price: 150,
+    brand: 'Nike',
+    rating: 3.5,
+    numReviews: 70,
+    countInStock: 2
+  }]
+};
+exports["default"] = _default;
