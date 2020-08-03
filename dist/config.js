1e19fc99d32aa6b6340222c938b14a0e568ac0f9
Ashique-Desai <desai.ashique@gmail.com>
2020-08-03 16:25:37 +0530

    rm again instead of rd

--- dist/config.js
+++ dist/config.js
@@ -14,6 +14,7 @@ _dotenv["default"].config();
 var _default = {
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+  PORT: process.env.PORT || 5000
 };
 exports["default"] = _default;
