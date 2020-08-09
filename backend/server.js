9f87d4c139c91605843023fcebff7ef6dff269e7
Ashique-Desai <desai.ashique@gmail.com>
2020-08-09 11:29:33 +0530

    server js changed

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,9 @@ app.get("/api/config/paypal", (req, res) => {
 })
 
 app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
-app.use(express.static(path.join(__dirname, '/../frontend/public')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../frontend/public/index.html`));
-});
+
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => { res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));});
 
 app.listen(config.PORT, () => {
   console.log('Server started at http://localhost:5000');
