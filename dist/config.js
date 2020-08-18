40a9e87b716a579e45d2e8ad3117ce684f737beb
Ashique-Desai <desai.ashique@gmail.com>
2020-08-18 19:52:55 +0530

    Config changed back

--- dist/config.js
+++ dist/config.js
@@ -13,7 +13,7 @@ _dotenv.default.config();
 
 var _default = {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://new_user-2:AYdiT9Ecj4BkbsW3@cluster0-7whwl.mongodb.net/amazona_data?retryWrites=true&w=majority',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
 };
