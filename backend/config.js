0f8e6ccee8e4c882f005433de5147784dd312198
Ashique-Desai <desai.ashique@gmail.com>
2020-08-19 12:00:15 +0530

    config changed back

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://new_user-2:AYdiT9Ecj4BkbsW3@cluster0-7whwl.mongodb.net/amazona_data?retryWrites=true&w=majority',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
 }
